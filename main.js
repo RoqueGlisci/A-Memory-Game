879a40ec939c8d66cdb01db9e105fbe43918c40b
roque <nico_4@live.com>
2022-05-10 14:40:53 -0300

    apply js

---
+++ main.js
@@ -0,0 +1,5 @@
+let number = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+//desordena el array de numeros. Math.random genera numero aleatorios entre 0 y 1
+number = number.sort(() => { return Math.random() - 0.5 });
+console.log(number);
